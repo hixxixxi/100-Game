3a617d69ed502aed64dd89e36e24ca962e338cbe
luckycham <48453186+luckycham@users.noreply.github.com>
2019-10-03 23:25:43 -0600

    Bouncing ball demo added
    Ball bounces upon it hitting a screen edge

---
+++ Object Movement/Assets/Scripts/Bounce.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Bounces the ball around the screen
+/// </summary>
+/// 
+/// Field           Description
+/// ball            The ball object to move
+/// 
+/// Author: Chamod Welhenge
+/// 
+public class Bounce : MonoBehaviour
+{
+    private Rigidbody2D ball;
+    public float ballSpeed = 4f;
+
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+    void Start()
+    {
+        // find the rigid body component
+        ball = GetComponent<Rigidbody2D>();
+        // Set the balls velocity
+        ball.velocity = new Vector2(ballSpeed, ballSpeed);
+    }
+}
