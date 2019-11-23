22977e1a534cb711aa257c52ebb44ae0386e745d
luckycham <48453186+luckycham@users.noreply.github.com>
2019-11-23 15:14:17 -0700

    Adding an array to hold all the generated circles

--- Assets/Scripts/Game/TargetGenerator.cs
+++ Assets/Scripts/Game/TargetGenerator.cs
@@ -6,11 +6,12 @@ using UnityEngine;
 /// Generates targets for the game
 /// </summary>
 /// 
-/// Field       Description
-/// myPrefab    Prefab of the object to be instantiated 
-/// limit       Limit of objects to be instantiated
-/// upForce     For random object force
-/// sideForce   For random object force
+/// Field           Description
+/// myPrefab        Prefab of the object to be instantiated 
+/// limit           Limit of objects to be instantiated
+/// upForce         For random object force
+/// sideForce       For random object force
+/// circlesList     List to hold all the circles
 /// 
 /// Source: https://www.youtube.com/watch?v=tdSmKaJvCoA (For calculating random forces)
 /// 
@@ -20,9 +21,10 @@ public class TargetGenerator : MonoBehaviour
 {
     // Reference to the Prefab.
     public GameObject myPrefab;
-    public float limit = 10;
+    public int limit = 10;
     public float upForce = 1f;
     public float sideForce = .1f;
+    public List<GameObject> circlesList;
 
     // Start is called before the first frame update
     void Start()
@@ -32,7 +34,7 @@ public class TargetGenerator : MonoBehaviour
             GameObject tmp;
             // Instantiate at position (0, 0, 0) and zero rotation.
             tmp = Instantiate(myPrefab, new Vector2(0, 0), Quaternion.identity);
-
+            circlesList.Add(tmp);
 
             // Random force vector
             float Xforce = Random.Range(-sideForce, sideForce);
