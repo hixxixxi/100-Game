aa1f893a388a79d31fb6ee5c53535522c80139f9
SeanLien13 <55466690+SeanLien13@users.noreply.github.com>
2019-11-26 12:54:41 -0700

    Randy--Added next level menu functionality
    Randy--Added next level menu functionality

---
+++ Assets/NextLevelMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextLevelMenu : MonoBehaviour
+{
+    public GameObject nextLevelUI;
+
+    public void NextLevel (){
+
+        // turn off menu 
+        nextLevelUI.SetActive(false);
+        Time.timeScale = 1f;
+
+        TargetGenerator.IncreaseDifficulty();
+
+    }
+}
---
+++ Assets/Scripts/TotalScoreScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TotalScoreScript : MonoBehaviour
+{
+    public static int totalScoreValue = 0;
+    public Text totalScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalScore = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        totalScore.text = "Total Score: " + totalScoreValue;
+    }
+}
