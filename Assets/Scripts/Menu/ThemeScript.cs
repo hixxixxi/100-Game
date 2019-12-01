ff89c2d740d0c21375ea3911d7b0bd2f57d3b1f5
luckycham <48453186+luckycham@users.noreply.github.com>
2019-12-01 13:53:04 -0700

    Theme selector added
    Christmas and space themed. Only the background change.

---
+++ Assets/Scripts/Menu/ThemeScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ThemeScript : MonoBehaviour
+{
+    public Sprite[] bg;
+    public int currentIndex = 0;
+    public Image image;
+
+    public void next() {
+
+        currentIndex++;
+        if (currentIndex >= bg.Length)
+        {
+            currentIndex = 0;
+        }
+        image.sprite = bg[currentIndex];
+        TargetGenerator.bgIn = image.sprite;
+
+    }
+
+
+    public void prev()
+    {
+        currentIndex--;
+        if(currentIndex < 0)
+        {
+            currentIndex = bg.Length - 1;
+
+        }
+        image.sprite = bg[currentIndex];
+        TargetGenerator.bgIn = image.sprite;
+    }
+
+}
