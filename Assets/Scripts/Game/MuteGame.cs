5f9a36f520a6d6d90f67f4a67c9468eceef35ff3
luckycham <48453186+luckycham@users.noreply.github.com>
2019-12-05 21:46:21 -0700

    Mute button added to the game scene
    game started unmuted

---
+++ Assets/Scripts/Game/MuteGame.cs
@@ -0,0 +1,12 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuteGame : MonoBehaviour
+{
+    public void MuteGameAudio()
+    {
+        AudioListener.pause = !AudioListener.pause; 
+    }
+}
+
